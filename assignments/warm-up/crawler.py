4b1dec65314c9e7eb98ffe157d59411e0dd67e8e
rani <raniking15@gmail.com>
2025-07-30 09:54:24 +0300

    refactor + style: seprated crawl function to smaller functions. renamed symbols.

--- assignments/warm-up/crawler.py
+++ assignments/warm-up/crawler.py
@@ -67,6 +67,70 @@ class Crawler:
             driver = webdriver.Chrome(options=self.options)
             self.driver = driver
 
+    @staticmethod
+    def extract_article_data(elems):
+        """
+        extracts title, description, date and image from the lady gaga google search page.
+        """
+        articles = []
+        for i in range(len(elems)):
+            data = {}
+            print(f"scraping article #{i}: ")
+            title = (
+                elems[i]
+                .find_element(by=By.CLASS_NAME, value="SoAPf")
+                .find_elements(by=By.TAG_NAME, value="div")[3]
+                .text
+            )
+            desc = (
+                elems[i]
+                .find_element(by=By.CLASS_NAME, value="SoAPf")
+                .find_elements(by=By.TAG_NAME, value="div")[4]
+                .text
+            )
+            date = (
+                elems[i]
+                .find_element(by=By.CLASS_NAME, value="SoAPf")
+                .find_element(by=By.XPATH, value=".//span[@data-ts]")
+                .get_attribute("data-ts")
+            )
+            date = int(str(date))
+            img = (
+                elems[i]
+                .find_element(by=By.CLASS_NAME, value="gpjNTe")
+                .find_element(by=By.TAG_NAME, value="div")
+                .find_element(by=By.TAG_NAME, value="div")
+                .find_element(by=By.TAG_NAME, value="img")
+                .get_attribute("src")
+            )
+            data["title"] = title if title else ""
+            data["desc"] = desc if desc else ""
+            data["image"] = img if img else ""
+
+            dt = datetime.fromtimestamp(date, tz=timezone.utc)
+            dt_local = dt.astimezone(ZoneInfo("Asia/Jerusalem"))
+            dt_local = dt_local.strftime("%d-%m-%Y, %H:%M:%S")
+            data["date"] = dt_local if date else ""
+
+            print(f"title: {title}")
+            print(f"desc: {desc}")
+            print(f"date: {dt_local}")
+            articles.append(data)
+        return articles
+
+    @staticmethod
+    def save_articles(articles):
+        """
+        Saves the articles to a .json format
+        """
+        serialized_data = json.dumps(articles)
+        print(f"Finished scraping {len(articles)} articles.")
+
+        with open("assignments/warm-up/scraped.json", "w") as f:
+            f.write(serialized_data)
+
+        return articles
+
     def crawl(self):
         """
         crawls the Lady Gaga url news page.
@@ -74,60 +138,9 @@ class Crawler:
         try:
             print(f"Navigating to {self.url}")
             self.driver.get(self.url)
-
             elems = self.driver.find_elements(by=By.CLASS_NAME, value="SoaBEf")
-            articles = []
-            for i in range(len(elems)):
-                data = {}
-                print(f"scraping article #{i}: ")
-                title = (
-                    elems[i]
-                    .find_element(by=By.CLASS_NAME, value="SoAPf")
-                    .find_elements(by=By.TAG_NAME, value="div")[3]
-                    .text
-                )
-                desc = (
-                    elems[i]
-                    .find_element(by=By.CLASS_NAME, value="SoAPf")
-                    .find_elements(by=By.TAG_NAME, value="div")[4]
-                    .text
-                )
-                date = (
-                    elems[i]
-                    .find_element(by=By.CLASS_NAME, value="SoAPf")
-                    .find_element(by=By.XPATH, value=".//span[@data-ts]")
-                    .get_attribute("data-ts")
-                )
-                date = int(date)
-                img = (
-                    elems[i]
-                    .find_element(by=By.CLASS_NAME, value="gpjNTe")
-                    .find_element(by=By.TAG_NAME, value="div")
-                    .find_element(by=By.TAG_NAME, value="div")
-                    .find_element(by=By.TAG_NAME, value="img")
-                    .get_attribute("src")
-                )
-                data["title"] = title if title else ""
-                data["desc"] = desc if desc else ""
-                data["image"] = img if img else ""
-
-                dt = datetime.fromtimestamp(date, tz=timezone.utc)
-                dt_local = dt.astimezone(ZoneInfo("Asia/Jerusalem"))
-                dt_local = dt_local.strftime("%d-%m-%Y, %H:%M:%S")
-                data["date"] = dt_local if date else ""
-
-                print(f"title: {title}")
-                print(f"desc: {desc}")
-                print(f"date: {dt_local}")
-                articles.append(data)
-
-            serialized_data = json.dumps(articles)
-            print(f"Finished scraping {len(articles)} articles.")
-
-            with open("assignments/warm-up/scraped.json", "w") as f:
-                f.write(serialized_data)
-
-            return articles
+            articles = self.extract_article_data(elems=elems)
+            self.save_articles(articles=articles)
 
         except Exception as e:
             print(f"Error during crawl: {e}")
@@ -138,7 +151,7 @@ class Crawler:
 
 
 if __name__ == "__main__":
-    crwler = Crawler(
+    crawler = Crawler(
         "https://www.google.com/search?q=lady+gaga+in+the+news&tbm=nws&source=univ&tbo=u&sa=X"
     )
-    crwler.crawl()
+    crawler.crawl()
--- assignments/warm-up/crawler_test.py
+++ assignments/warm-up/crawler_test.py
@@ -3,25 +3,26 @@ import base64
 import binascii
 from crawler import Crawler
 
+
 class TestCrawler(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("Running Init")
-        crwler = Crawler(
-                    "https://www.google.com/search?q=lady+gaga+in+the+news&tbm=nws&source=univ&tbo=u&sa=X"
-                )
-        cls.articles = crwler.crawl()
+        crawler = Crawler(
+            "https://www.google.com/search?q=lady+gaga+in+the+news&tbm=nws&source=univ&tbo=u&sa=X"
+        )
+        cls.articles = crawler.crawl()
 
     def test_title(self):
         for art in self.articles:
-            self.assertIsNotNone(art['title'])
+            self.assertIsNotNone(art["title"])
 
     def test_desc(self):
         for art in self.articles:
-            self.assertIsNotNone(art['desc'])
+            self.assertIsNotNone(art["desc"])
 
     @staticmethod
-    def is_base64(s:str):
+    def is_base64(s: str):
         try:
             # Try decoding (with padding correction)
             base64.b64decode(s, validate=True)
@@ -31,10 +32,9 @@ class TestCrawler(unittest.TestCase):
 
     def test_img(self):
         for art in self.articles:
-            self.assertTrue(self.is_base64(
-                art['image']
-                .removeprefix("data:image/jpeg;base64,")
-            ))
+            self.assertTrue(
+                self.is_base64(art["image"].removeprefix("data:image/jpeg;base64,"))
+            )
 
 
 if __name__ == "__main__":
