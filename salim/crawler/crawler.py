2dbcfdc1d66ad6005deab27e627c2e81b1fb69eb
julio1703 <julio.elias22@gmail.com>
2025-07-31 11:13:02 +0300

    added a empty json file and a skeleton for the crawler

---
+++ salim/crawler/crawler.py
@@ -0,0 +1,24 @@
+class Crawler:
+    def __init__(self):
+        pass
+
+    def crawl(self):
+        """
+        Fetch and process data from the target source.
+        """
+        pass
+
+    def save_file(self, data, filename):
+        """
+        Save the given data to a local file.
+        :param data: The data to be saved
+        :param filename: Name of the file to save the data in
+        """
+        pass
+
+    def upload_file(self, filepath):
+        """
+        Upload the file to a remote destination (e.g., cloud storage).
+        :param filepath: Path to the file to be uploaded
+        """
+        pass
