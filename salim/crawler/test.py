dfcc96f15407a6c5853ed15e50dcfdf8cd2a5c02
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2025-08-06 19:30:25 +0300

    Updating test file

--- salim/crawler/test.py
+++ salim/crawler/test.py
@@ -1,6 +1,5 @@
-from browser_utils import *
+from crawler import Crawler
 
-if __name__ == "__main__":
-    driver = get_chromedriver()
-    soup = get_html_parser(driver, "https://www.gov.il/he/pages/cpfta_prices_regulations")
-    print(f"HTML: {soup}")
+if name == "main":
+    crawler = Crawler()
+    crawler.crawl() 
