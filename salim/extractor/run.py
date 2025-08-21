948415d4f9433e5a6f0935f54f7eed034c1c3431
julio1703 <julio.elias22@gmail.com>
2025-08-21 10:54:07 +0300

    feat: added the stores json files

--- salim/extractor/run.py
+++ salim/extractor/run.py
@@ -1,7 +1,9 @@
 from extractor import Extractor
 from sqs.sqs_producer import receive_messages_from_sqs, send_message_to_sqs
 import json
+from utils import *
 
 if __name__ == "__main__":
-    extractor = Extractor()
-    extractor.extract_from_s3()
+    # extractor = Extractor()
+    # extractor.extract_from_s3()
+    convert_xml_to_json(r"stores\dcarrefour\Stores7290055700007-202508210001.xml")
