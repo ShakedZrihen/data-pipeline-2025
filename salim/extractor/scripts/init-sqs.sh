e8cdfe4fd2e8428e2b1074344a546d16c5ead923
yoav1870 <yoav18700@gmail.com>
2025-08-31 23:29:57 +0300

    sqs + stating of poll , script to init the sqs .

---
+++ salim/extractor/scripts/init-sqs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Initializing SQS queue..."
+sleep 5
+awslocal sqs create-queue --queue-name sqs-queue 
+QUEUE_URL=$(awslocal sqs get-queue-url --queue-name sqs-queue --output text)
+echo "Queue URL: $QUEUE_URL"
+echo "SQS initialization completed!"
